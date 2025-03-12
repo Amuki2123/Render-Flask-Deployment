bd2c0926ef493261db864bd78c701fd976b58ae1
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2025-03-12 12:02:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,5 @@ def download():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    # Use the PORT environment variable or default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
 
